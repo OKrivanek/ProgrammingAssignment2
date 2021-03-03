84306c433fb9a59839288470f41d42a359418133
OKrivanek <79971113+OKrivanek@users.noreply.github.com>
2021-03-03 10:30:57 +0100

    ProgrammingAssignmentOK

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,18 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+makeCacheMatrix <- function(x = matrix()) {
+  pomocna <- NULL
+  set <- function(y) {    # initialize to load a matric
+    x <<- y
+    pomocna <<- NULL 
+  }
+  get <- function() x     
+  setInverse <- function() pomocna <<- solve(x) #using solve function get inverse matrix under assumption that a matrix is always invertible
+  getInverse <- function() pomocna # Return a matrix that is the inverse of 'x'
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse) 
+}
